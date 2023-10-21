dae19d3cb65e19b633ca8ba15865fccc33803336
rezaur-rahman-rasul <md.rezaur.rahman.rasul@gmail.com>
2023-10-21 15:10:02 +0600

    Grails Project Modification

---
+++ grails-app/services/com/reza/ocb/MemberService.groovy
@@ -0,0 +1,4 @@
+package com.reza.ocb
+
+class MemberService {
+}
---
+++ src/main/groovy/com/reza/ocb/GlobalConfig.groovy
@@ -0,0 +1,8 @@
+package com.reza.ocb
+
+class GlobalConfig {
+    public static final def USER_TYPE = [
+            ADMINISTRATOR : "ADMINISTRATOR",
+            REGULAR_MEMBER: "REGULAR_MEMBER",
+    ]
+}
