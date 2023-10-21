f924be9f580f33bd37cfafd1f2c73f76c5a992e3
rezaur-rahman-rasul <md.rezaur.rahman.rasul@gmail.com>
2023-10-21 15:14:11 +0600

    Grails Project Modification

--- grails-app/services/com/reza/ocb/MemberService.groovy
+++ grails-app/services/com/reza/ocb/MemberService.groovy
@@ -1,4 +1,61 @@
 package com.reza.ocb
 
+import grails.web.servlet.mvc.GrailsParameterMap
+import org.springframework.validation.BindingResult
+
 class MemberService {
+    def save(GrailsParameterMap params) {
+        Member member = new Member(params)
+        def response = AppUtil.saveResponse(false, member)
+        if (member.validate()) {
+            member.save(flush: true)
+            if (!member.hasErrors()){
+                response.isSuccess = true
+            }
+        }
+        return response
+    }
+
+
+    def update(Member member, GrailsParameterMap params) {
+        member.properties = params as BindingResult
+        def response = AppUtil.saveResponse(false, member)
+        if (member.validate()) {
+            member.save(flush: true)
+            if (!member.hasErrors()){
+                response.isSuccess = true
+            }
+        }
+        return response
+    }
+
+
+    def getById(Serializable id) {
+        return Member.get(id)
+    }
+
+    def list(GrailsParameterMap params) {
+        params.max = params.max ?: GlobalConfig.itemsPerPage()
+        List<Member> memberList = Member.createCriteria().list(params) {
+            if (params?.colName && params?.colValue) {
+                like(params.colName, "%" + params.colValue + "%")
+            }
+            if (!params.sort) {
+                order("id", "desc")
+            }
+        } as List<Member>
+        return [list: memberList, count: memberList.totalCount]
+    }
+
+
+    def delete(Member member) {
+        try {
+            member.delete(flush: true)
+        } catch (Exception e) {
+            println(e.getMessage())
+            return false
+        }
+        return true
+    }
+
 }
